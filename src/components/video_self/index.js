d9ccdfe5e0b3df1764b1e637e545372ba4bbba10
libingyang <libingyang@xingfeiinc.com>
2021-01-28 21:32:32 +0800

    feat:改变App的宽度

---
+++ src/components/video_self/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './index.css'
+const VideoSelf = () => {
+    return (
+        <video className='video_box' src='https://dev-oss.gramaker.com/developer/free/material%E8%88%9E%E5%8F%B0-1611817500732.mp4' />
+    )
+}
+
+export default VideoSelf;
